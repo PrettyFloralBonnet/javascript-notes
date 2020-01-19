eaac06372ee29242731775c373eb1662c96dbd5c
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2020-01-19 22:21:03 +0100

    completed logical operators section

--- fundamentals.js
+++ fundamentals.js
@@ -105,7 +105,9 @@ Number('hello')  // NaN
 Boolean('0')  // true (string is not empty)
 const FALSY_VALUES = [0, '', null, undefined, NaN];
 
-// ----- INCREMENT/DECREMENT -----
+// ----- OPERATORS -----
+
+// increment/decrement
 
 // operators ++ and -- can be placed before ('prefix') and after ('postfix') a variable
 let counter = 1;
@@ -117,8 +119,208 @@ let counter = 1;
 let a = counter++;  // 'postfix' form increments counter but returns old value
 alert(a);  // 1
 
-// ----- BITWISE OPERATORS -----
+// bitwise operators
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators
 
 // Bitwise operators treat arguments as 32-bit integers.
 // They work on the level of their binary representation,
 // but return standard JavaScript numbers.
+
+// comma
+
+// The comma operator allows us to evaluate several expressions:
+
+let a = (1 + 2, 3 + 4);
+console.log(a);  // 7; 1 + 2 is evaluated and immediately discarded
+
+// Each of them is evaluated, but only the result of the last one is returned.
+// Comma has a very low precedence (lower than '='), so parentheses are needed in the example above.
+
+// ----- COMPARISONS -----
+
+// for string comparison, JavaScript uses a dictionary/lexographical order
+// where strings are compared letter by letter:
+
+console.log( 'Z' > 'A' );  // true
+console.log( 'Glow' > 'Glee' );  // true
+console.log( 'Bee' > 'Be' );  // true
+
+// lowercase characters have greater indices in Unicode than uppercase ones
+
+// it's possible that two values are equal, but one of them is true as boolean, whereas the other is false
+
+let a = 0;
+console.log( Boolean(a) );  // false
+let b = "0";
+console.log( Boolean(b) );  // true
+
+console.log(a == b);  // true!
+
+// strict equality operator
+
+/// The '===' operator checks for equality without type conversion:
+
+console.log(null === undefined);  // false
+
+// null and undefined
+
+// the standard (non-strict) equality check '==' for undefined and null is defined
+// so that (without any conversions) they are equal to each other, and are not equal to anything else:
+
+console.log(null == 0);  // false
+console.log(null == undefined);  // true
+
+// NaN in comparisons
+
+console.log(NaN == NaN);  // false
+
+// NaN returns false for all comparisons. To check if a value is a NaN, use isNaN() function.
+
+// ----- INTERACTION: ALERT, PROMPT, CONFIRM -----
+
+// alert()
+
+let message = '!';
+alert(message);
+
+// This shows a message in a modal window and pauses script execution until 'OK' is pressed
+
+// prompt()
+
+result = prompt(title, _default);
+
+// 'title' is the text to show to the user
+// _default is the optional default value of the input field
+
+// prompt() returns the text from the input field or null if the input was cancelled
+
+// confirm()
+
+question = 'Is this podracing?'
+result = confirm(`${question}`);
+
+// This shows a modal window with the question and two buttons: OK and Cancel
+// Returns true if OK was pressed and false otherwise
+
+// ----- CONDITIONAL OPERATORS -----
+
+// TASK: Using if..else, write the code which gets a number via prompt, and then alerts:
+
+//     1, if the value is greater than zero,
+//     -1, if it's less than zero,
+//     0, if it equals zero.
+
+// -->
+let num = prompt('Enter a number', 0);
+
+if (!isNaN(+num)) {
+    if (num > 0) {
+        alert(1);
+    } else if (num < 0) {
+        alert(-1);
+    } else {
+        alert(0);
+    }
+} else {
+    alert('Could not evaluate input as a number.');
+};
+
+// TASK: Rewrite the code below using the conditional operator '?':
+
+let result;
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+
+// -->
+let result = (a + b < 4) ? 'Below' : 'Over';
+
+// ----- LOGICAL OPERATORS -----
+
+// There are three logical operators in JavaScript: || (OR), && (AND), ! (NOT).
+
+// || (OR)
+
+// a chain of OR "||" returns the first truthy value, or the last one if no truthy value is found:
+
+alert( 1 || 0 );  // 1
+alert( true || 'no matter what' );  // true
+alert( null || 1 );  // 1
+alert( null || 0 || 1 );  // 1
+alert( undefined || null || 0 );  // 0 (returns the last value because all are falsy)
+
+// && (AND)
+
+// a chain of AND "&&" returns the first falsy value, or the last value if none were found:
+
+alert( 1 && 0 );  // 0
+alert( 1 && 5 );  // 5
+alert( null && 5 );  // null
+alert( 0 && "no matter what" );  // 0
+alert( 1 && 2 && null && 3 );  // null
+alert( 1 && 2 && 3 );  // 3 (the last one)
+
+// ! (NOT)
+
+// The NOT "!" operator converts the operand to boolean and returns the inverse value.
+
+alert( !true );  // false
+alert( !!"non-empty string" );  // true (note the two '!' operators)
+
+// Precedence of NOT is lower than OR is lower than AND.
+
+
+// TASK: Write a conditional statement to check that age is NOT between 14 and 90 inclusively.
+// Create two variants: the first one using NOT !, the second one – without it.
+// -->
+
+let age = prompt('Enter your age', '');
+
+if (!(age >= 14 && age <= 90)) {
+  alert('Age condition met.');
+} else {
+  alert('Age condition not met.');
+};
+
+let age = prompt('Enter your age', '');
+
+if ((age < 14) || (age > 90)) {
+  alert('Age condition met.');
+} else {
+  alert('Age condition NOT met.');
+};
+
+
+// TASK: Write the code asking for a login with prompt.
+// If the input is "Admin" - prompt for a password.
+// If the input is empty (or Esc) - show “Login cancelled.”
+// If it’s something else - show “Incorrect username.”
+
+// The password is checked as follows:
+
+//     If it equals “TheMaster”, then show “Welcome!”
+//     Another string – show “Incorrect password.”
+//     For an empty string or cancelled input, show “Login cancelled.”
+// -->
+
+let login = prompt('Enter username:', '');
+
+if (login == 'Admin') {
+    let password = prompt('Enter password:', '');
+
+    if (password == 'TheMaster') {
+        alert('Welcome!');
+    } else if (!password) {
+        alert('Login cancelled.');
+    } else {
+        alert('Incorrect password.');
+    };
+} else if (!login) {
+    alert('Login cancelled');
+} else {
+    alert('Incorrect username.');
+};
