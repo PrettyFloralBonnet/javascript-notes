2914b84630a43932b4f9f7e1c52a268897c2c099
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2020-02-03 22:50:41 +0100

    start data types chapter

---
+++ dataTypes.js
@@ -0,0 +1,13 @@
+// ----- METHODS OF PRIMITIVE DATA TYPES -----
+
+// JavaScript allows us to work with primitive data types (strings, numbers etc.) as if they were objects.
+// To that end, a special object wrapper is created for the primitive data type,
+// providing access to methods and properties. Then it is destroyed.
+
+// null and undefined
+
+// These two types do not have corresponding wrapper objects and provide no methods.
+
+// ----- NUMBERS -----
+
+// wip
