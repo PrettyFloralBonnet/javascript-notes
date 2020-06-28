743a94961b6e298846920693965cd9d3dad3ac3c
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2020-06-28 13:24:50 +0200

    continue function object section

--- advancedFunctions.js
+++ advancedFunctions.js
@@ -879,7 +879,7 @@ if (!window.Promise) {
 // pass by reference etc.
 
 // Functions have built-in properites such as name (default ones are created based on context), length
-// (returns the number of parameters the function has).
+// (returns the number of parameters the function has, not counting rest parameters).
 
 // Custom properties can be added as well. Note that a property assigned to a function, e.g. sayHi.counter = 0,
 // does not define a local variable "counter" inside it. In other words, a "counter" property and a "counter" variable
@@ -917,3 +917,63 @@ sayHi = null;
 // and so it solves that problem.
 
 // NFE syntax does not exist for function declarations.
+
+// Many widely used JavaScript libraries make use of the NFE feature by creating a "main" function and attaching "helper"
+// functions to it (so that the function is useful by itself, but also carries other functionalities as properties).
+// E.g. the jQuery library creates a function named $, and the lodash library creates a function _ (and then adds _.clone,
+// _.keyBy and other properties to it). Doing so also mitigates the pollution of the global space, so that one library
+// only provides a single global variable, reducing the possibility of name conflicts.
+
+// TASK: Modify the code below so that the counter can also be decreased and set the number:
+//
+// - counter() should return the next number (as before)
+// - counter.set(value) should set the counter to value
+// - counter.decrease() should decrease the counter by 1
+
+function makeCounter() {
+    function counter() {
+        return counter.count++;
+    };
+
+    counter.count = 0;
+    return counter;
+}
+
+let counter = makeCounter();
+
+// Use either a closure or a function property to keep the current count (or write both variants).
+// -->
+
+function makeCounter() {
+    function counter() {
+        return counter.count++;
+    };
+
+    counter.set = function(value) {
+        counter.count = value;
+        return counter.count;
+    };
+
+    counter.decrease = function() {
+        return counter.count--;
+    };
+
+    counter.count = 0;
+    return counter;
+}
+
+// ...or:
+
+function makeCounter() {
+    let count = 0;
+
+    function counter() {
+        return count++;
+    }
+
+    counter.set = value => count = value;
+
+    counter.decrease = () => count--;
+
+    return counter;
+}
