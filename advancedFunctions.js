9eddd989a207f321755c8e02eee211bbe4685869
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2020-07-28 23:50:50 +0200

    continue decorators and forwarding section

--- advancedFunctions.js
+++ advancedFunctions.js
@@ -1180,3 +1180,97 @@ console.log(slow(2));  // slow(2) is cached
 
 // We can call cachingDecorator for any function, and it will return the caching wrapper.
 // By separating caching from the main function code, we keep the latter more simple.
+
+// Using func.call for the context
+
+// The caching decorator is not suited to work with object methods. E.g. in the code below,
+// worker.slow() stops working:
+
+let worker = {
+    someMethod() {
+        return 1;
+    },
+
+    slow(x) {
+        // CPU-heavy task here
+        console.log("Called with " + x);
+        return x * this.someMethod();
+    }
+};
+
+function cachingDecorator(func) {
+    let cache = new Map();
+    return function (x) {
+        if (cache.has(x)) {
+            return cache.get(x);
+        }
+        let result = func(x);
+        cache.set(x, result);
+        return result;
+    };
+}
+
+console.log(worker.slow(1));  // the original method works
+worker.slow = cachingDecorator(worker.slow);
+console.log(worker.slow(2));  // Error: Cannot read property 'someMethod' of undefined
+
+// The error occurs when the code tries to access this.someMethod and fails.
+// The reason is that the wrapper calls the original function as func(x) in the line,
+// and the function receives *this* equal to undefined.
+
+// So, the wrapper passes the call to the original method, but without the context. Let’s fix it.
+
+// There's a special built-in function method func.call(context, ...args) that allows to call a function
+// and explicitly set *this*:
+
+// func.call(context, arg1, arg2, ...)
+
+// It runs func and provides the first argument as *this*.
+// These two calls do almost the same:
+
+func(1, 2, 3);
+func.call(obj, 1, 2, 3)
+
+// They both call func with arguments 1, 2 and 3. The only difference is that func.call also sets *this* to obj.
+
+// We can call sayHi in the context of different objects: sayHi.call(user):
+
+function sayHi() {
+    console.log(this.name);
+}
+
+let user = { name: "John" };
+let admin = { name: "Admin" };
+
+// use call to pass different objects as *this*
+sayHi.call(user);  // John
+sayHi.call(admin);  // Admin
+
+// So, we can use call in the wrapper to pass the context to the original function:
+
+let worker = {
+    someMethod() {
+        return 1;
+    },
+
+    slow(x) {
+        console.log("Called with " + x);
+        return x * this.someMethod();
+    }
+};
+
+function cachingDecorator(func) {
+    let cache = new Map();
+    return function (x) {
+        if (cache.has(x)) {
+            return cache.get(x);
+        }
+        let result = func.call(this, x);  // *this* is passed correctly now
+        cache.set(x, result);
+        return result;
+    };
+}
+
+worker.slow = cachingDecorator(worker.slow);
+console.log(worker.slow(2));  // works
+console.log(worker.slow(2));  // works, doesn't call the original (cached)
